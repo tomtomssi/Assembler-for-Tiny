5a32c161d4ea20024382e27856bb2461e3d23f17
Tatu Solla <tatu.solla@live.com>
2014-03-24 00:46:32 +0200

    Finished the opcode list

--- Parser.java
+++ Parser.java
@@ -17,13 +17,34 @@ public class Parser {
 
     public Parser() {
         initOPCODE();
-        int x = (int)_Opcode.get("OR");
+        int x = (int) _Opcode.get("OR");
     }
-    
-    public void initOPCODE(){
-        _Opcode.put("AND", 1);
+
+    public void initOPCODE() {
+        //Logic
+        _Opcode.put("AND", 0x00);
         _Opcode.put("OR", 0x02);
-        _Opcode.put("XOR", "0x04");
-        _Opcode.put("NOT", "0x06");
+        _Opcode.put("XOR", 0x04);
+        _Opcode.put("NOT", 0x06);
+
+        //Memory
+        _Opcode.put("MOV", 0x0);
+
+        //Math
+        _Opcode.put("RANDOM", 0x09);
+        _Opcode.put("ADD", 0x0a);
+        _Opcode.put("SUB", 0x0c);
+
+        //Control
+        _Opcode.put("JMP", 0x0e);
+        _Opcode.put("JZ", 0x10);
+        _Opcode.put("JEQ", 0x14);
+        _Opcode.put("JLS", 0x18);
+        _Opcode.put("JGT", 0x1c);
+        _Opcode.put("HALT", 0xff);
+
+        //Utilities
+        _Opcode.put("APRINT", 0x20);
+        _Opcode.put("DPRINT", 0x22);
     }
 }
